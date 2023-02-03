169c1f998602fa12acc44094ee6f2f97f0efd5c0
jeff <jeff@gmail.com>
2023-02-03 12:37:02 +0100

    fonctionne + css

--- src/partials/menu.php
+++ src/partials/menu.php
@@ -12,82 +12,79 @@
 		margin-bottom: 5%;
 		">
 		<?php
-		if ($user) {
+			if ($user) {
 			?>
 			<li>
-       verifyingTest
-        <a href="/index.php"  style="
+       			<a href="/index.php"  style="
                     color: black;
                     margin-right: 50px;
                     font-size: 20px;
                     font-weight: bolder;
                     text-decoration:none;
-                  ">Accueil</a>
+                ">Accueil</a>
                   
 				<a href="/transactions.php"  style="
-						color: black;
-						margin-right: 50px;
-						font-size: 20px;
-						font-weight: bolder;
-						text-decoration:none;
-					">Transactions</a>
-          
-		<?php } ?>
-		<?php if ($user && $role>1) { ?>
-			<a href="/operation.php"  style="
+					color: black;
+					margin-right: 50px;
+					font-size: 20px;
+					font-weight: bolder;
+					text-decoration:none;
+				">Transactions</a>
+				<?php 
+				if ($user && $role>1) { 
+				?>
+					<a href="/operation.php"  style="
 						color: black;
 						margin-right: 50px;
 						font-size: 20px;
 						font-weight: bolder;
 						text-decoration:none;
 					">Opération</a>
-          
-			<a href="/admin.php"  style="
+			
+					<a href="/admin.php"  style="
 						color: black;
 						margin-right: 50px;
 						font-size: 20px;
 						font-weight: bolder;
 						text-decoration:none;
 					">Admin</a>
-          
-				<a href="/users.php">Utilisateurs</a>
-		<?php } ?>
-		<?php if ($user && $role == 100) { ?>
-    
-				<a href="/upgrade.php">Upgrade user</a>
-        
-			<?php } ?>
-      <?php
-				if ($user) {
-			?>
-				<a href="/actions/logout.php"  style="
-						color: black;
-						margin-right: 50px;
-						font-size: 20px;
-						font-weight: bolder;
-						text-decoration:none;
-					">Logout</a>
-
-			<?php } ?>
-	<?php } else { ?>
-			<div class="nav">
-				
-					<a href="/login.php" style="
-						color: black;
-						margin-right: 50px;
-						font-size: 20px;
-						font-weight: bolder;
-						text-decoration:none;
-					">Connexion</a>
-					
-					<a href="/register.php" style="
-						color: black;
-						margin-right: 50px;
-						font-size: 20px;
-						font-weight: bolder;
-						text-decoration:none;">Créer un compte</a>
-				</li>
-			</div>
-		<?php } ?>
-  </ul>
+			
+					<a href="/users.php">Utilisateurs</a>
+				<?php } ?>
+				<?php 
+					if ($user && $role == 100) { 
+					?>
+						<a href="/upgrade.php">Upgrade user</a>
+				<?php } ?>
+				<?php
+					if ($user) {
+					?>
+						<a href="/actions/logout.php"  style="
+								color: black;
+								margin-right: 50px;
+								font-size: 20px;
+								font-weight: bolder;
+								text-decoration:none;
+							">Logout</a>
+				<?php } ?>
+			<?php } else {?>
+					<div class="nav">
+							<a href="/login.php" style="
+								color: black;
+								margin-right: 50px;
+								font-size: 20px;
+								font-weight: bolder;
+								text-decoration:none;
+							">Connexion</a>
+							
+							<a href="/register.php" style="
+								color: black;
+								margin-right: 50px;
+								font-size: 20px;
+								font-weight: bolder;
+								text-decoration:none;">Créer un compte</a>
+					</li>
+					</div>
+				<?php } ?>
+  	</ul>
 </body>
